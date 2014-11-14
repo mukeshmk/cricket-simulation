9dcddadc4a9ce9c1d0f182151a3df9ebfc0826bd
Mukesh <amukesh.mk@gmail.com>
2014-11-15 00:18:05 +0530

    Starting
    creating classes and stuff

---
+++ project.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include<stdlib>
+using namespace std;
+class bowling
+{
+	private:
+		int bwl_pts;
+		char bwl_typ[30];
+		float eco;
+		int wkts;
+		float spd;
+	public:
+		bowling()
+		{
+			bwl_pts=0;
+			strcpy(b
+}
