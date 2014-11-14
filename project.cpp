17d6a9559394edd7754c669759c2e3ef9cd9ba61
Mukesh <amukesh.mk@gmail.com>
2014-11-15 01:25:44 +0530

    bat class starting
    bat class just started by Yash

--- project.cpp
+++ project.cpp
@@ -64,3 +64,57 @@ ofstream& operator << (ofstream& out,bowling b)
 	out<<"wickets taken: "<<b.wkts<<endl;
 	out<<"bowling speed: "<<b.speed<<endl;
 }
+class bat {
+    //members:
+	private:
+		int bat_points;
+		char bat_type[30];     
+		int high_score;     
+		float average;//str rate     
+		int cent;     
+		int half_cent;     
+		int score;     
+		int num_ball;     
+	//functions:     
+	public:         
+		void bat()      //default constructor       
+		{             
+			bat_points=0;             
+			high_score=0;             
+			average=0.0;             
+			cent=0;             
+			half_cent=0;             
+			score=0;             
+			num_ball=0;             
+			strcpy(bat_type,"");         
+		}         
+		friend istream& operator >> (istream& in,bat b); //operator overloading extraction         
+		friend ostream& operator << (ostream& out,bat b);//operator overloading insertion         
+		void change_stats(int scr)         
+		{             
+			num_ball++;             
+			if(!scr=-1)             
+				score+=scr;             
+			average=score/num_ball;          
+		}  
+}; 
+istream& operator >>(istream& in, bat b) 
+{         
+	cout<<"Enter batsman's points: "<<endl;         
+	in>>b.bat_points;         
+	cout<<"Enter batting type: "<<endl;         
+	in>>b.bat_type;         
+	cout<<"Enter batsman's high score: "<<endl;         
+	in>>b.high_score;         
+	cout<"Enter number of centuries and half-centuries: "<<endl;         
+	in>>b.cent>>b.half_cent;  
+} 
+ostream& operator <<(ostream& out, bat b) 
+{     
+	out<<"Number of Balls: "<<b.num_ball<<endl;     
+	out<<"Batsman's points: "<<b.bat_points<<endl;     
+	out<<"Batting type: "<<b.bat_type<<endl;     
+	out<<"High score: "<<b.high_score<<endl;     
+	out<<"Centuries: "<<b.cent<<endl;     
+	out<<"Half Centuries: "<<b.half_cent<<endl; 
+}
