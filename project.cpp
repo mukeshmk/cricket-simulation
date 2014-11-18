5bbb2d2c1a69e1208cf20f0a148d3fc1f6cbc30a
pingyash <pingyash96@gmail.com>
2014-11-18 22:40:56 +0530

    bug fixes - 01

--- project.cpp
+++ project.cpp
@@ -13,17 +13,18 @@ int arr[4][10]={{-1,-1,-1,0,1,2,4,4,6,6},{-1,-1,1,2,2,4,4,6,6,6},{-1,-1,-1,0,0,1
 
 class bowl
 {
-	private:	
+	private:
 		int bwl_pts;
 		char bwl_typ[30];
-		
-		int balls;
+
+
 		int runs;
 		float eco;
 		int wkts;
 		float spd;
 		friend class team;
 	public:
+	    int balls;
 		bowl()
 		{
 			bwl_pts=0;
@@ -81,66 +82,66 @@ class bat {
     //members:
 	private:
 		int bat_points;
-		char bat_type[30];     
-		int cent;     
-		int half_cent;     
-		
-		int high_score;     
-		float average;//str rate     
-		int score;     
-		int num_ball;    
+		char bat_type[30];
+		int cent;
+		int half_cent;
+
+		int high_score;
+		float average;//str rate
+		int score;
+		int num_ball;
 		friend class team;
-	//functions:     
-	public:         
-		bat()      //default constructor       
-		{             
-			bat_points=0;             
-			high_score=0;             
-			average=0.0;             
-			cent=0;             
-			half_cent=0;             
-			score=0;             
-			num_ball=0;             
-			strcpy(bat_type,"");         
-		}         
+	//functions:
+	public:
+		bat()      //default constructor
+		{
+			bat_points=0;
+			high_score=0;
+			average=0.0;
+			cent=0;
+			half_cent=0;
+			score=0;
+			num_ball=0;
+			strcpy(bat_type,"");
+		}
 
 		void get_bat();
 		void put_bat();
 
-		void bat_cs(int scr)         
-		{             
-			num_ball++;             
-			if(!(scr==-1))             
-				score+=scr;             
-			average=score/num_ball;          
+		void bat_cs(int scr)
+		{
+			num_ball++;
+			if(!(scr==-1))
+				score+=scr;
+			average=score/num_ball;
 		}
 
 		int ret_batpts()
 		{
 			return bat_points;
 		}
-}; 
+};
 
 void bat::get_bat()
-{         
-	cout<<"Enter batsman's points: "<<endl;         
-	cin>>bat_points;         
-	cout<<"Enter batting type: "<<endl;         
-	cin>>bat_type;         
-	cout<<"Enter batsman's high score: "<<endl;         
-	cin>>high_score;         
-	cout<<"Enter number of centuries and half-centuries: "<<endl;         
-	cin>>cent>>half_cent;  
-} 
+{
+	cout<<"Enter batsman's points: "<<endl;
+	cin>>bat_points;
+	cout<<"Enter batting type: "<<endl;
+	cin>>bat_type;
+	cout<<"Enter batsman's high score: "<<endl;
+	cin>>high_score;
+	cout<<"Enter number of centuries and half-centuries: "<<endl;
+	cin>>cent>>half_cent;
+}
 
 void bat::put_bat()
-{     
-	cout<<"Number of Balls: "<<num_ball<<endl;     
-	cout<<"Batsman's points: "<<bat_points<<endl;     
-	cout<<"Batting type: "<<bat_type<<endl;     
-	cout<<"High score: "<<high_score<<endl;     
-	cout<<"Centuries: "<<cent<<endl;     
-	cout<<"Half Centuries: "<<half_cent<<endl<<endl; 
+{
+	cout<<"Number of Balls: "<<num_ball<<endl;
+	cout<<"Batsman's points: "<<bat_points<<endl;
+	cout<<"Batting type: "<<bat_type<<endl;
+	cout<<"High score: "<<high_score<<endl;
+	cout<<"Centuries: "<<cent<<endl;
+	cout<<"Half Centuries: "<<half_cent<<endl<<endl;
 }
 
 class player:public bat,public bowl
@@ -184,25 +185,25 @@ class team
 	private:
 		static int tno;
 		int teamno;
-		char team_nm[30];         
+		char team_nm[30];
 		player p[11];
 		friend class player;
-	public:         
-		team()//default constructor         
-		{             
+	public:
+		team()//default constructor
+		{
 			strcpy(team_nm,"");
 			tno++;
 			teamno=tno;
 		}
-		void read_playerinfo();		
-		void input()         
-		{             
-			cout<<teamno<<"Enter the team's name: "<<endl;             
+		void read_playerinfo();
+		void input()
+		{
+			cout<<teamno<<"Enter the team's name: "<<endl;
 			cin>>team_nm;
 			read_playerinfo();
-		}         
-		void output()         
-		{             
+		}
+		void output()
+		{
 			cout<<"The team's name: "<<team_nm<<endl<<endl;
 			for(int i=0;i<11;i++)
 				p[i].put_player();
@@ -279,7 +280,8 @@ int main()
 	t1.input();
 	t2.input();
 	save_file(t1,t2);
-	t1.output();
-	t2.output();
+
+	//t1.output();
+	//t2.output();
 	return 0;
 }
